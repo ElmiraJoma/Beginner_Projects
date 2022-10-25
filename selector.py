fce6b009532f6806c40d9c706644e95e45c4db59
Elmira Joma <116452604+ElmiraJoma@users.noreply.github.com>
2022-10-25 23:38:59 +0300

    Add files via upload

---
+++ selector.py
@@ -0,0 +1,20 @@
+import random
+
+friends = [
+    'Ivan',
+    'Valentina',
+    'Elisaveta',
+    'Alexandra',
+    'Mariya',
+    'Fadi',
+    'Peter',
+    'Katerina',
+    'Nadya'
+]
+
+
+
+selected = random.choice(friends) 
+
+print('Who should I facetime today?')
+print(selected)
